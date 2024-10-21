76af387168e0bda46f7d5b79637e2f76830f8e4f
olaguru <dapoodedeyi03@gmail.com>
2024-10-21 06:12:11 -0700

    test

--- markdown2html.py
+++ markdown2html.py
@@ -2,9 +2,11 @@
 """ Markdown to Html"""
 import sys
 import os
+""" sys for args and os to check file"""
 
 
 if len(sys.argv) < 3:
+    """ confirm the args coming in"""
     sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
     sys.exit(1)
 
@@ -13,6 +15,7 @@ htmlFile = sys.argv[2]
 
 
 if not os.path.isfile(markdownFile):
+    """check markdown if it is a markdownfile"""
     sys.stderr.write(f'Missing {mardownFile}\n')
     sys.exit(1)
 
