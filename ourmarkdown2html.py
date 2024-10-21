bb5da9dc163e905645d2a6799f2200dd23cb0879
olaguru <dapoodedeyi03@gmail.com>
2024-10-21 06:48:19 -0700

    test

--- ourmarkdown2html.py
+++ ourmarkdown2html.py
@@ -9,19 +9,20 @@ import sys
 import os
 """ sys for args and os to check file"""
 
+def check_markdown(markdownFile, htmlFile):
+    """ markdown to html"""
+    if len(sys.argv) != 3:
+        """ check the arg len """
+        sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
+        sys.exit(1)
 
-if len(sys.argv) != 3:
-    """ check the arg len """
-    sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
-    sys.exit(1)
-
-markdownFile = sys.argv[1]
-htmlFile = sys.argv[2]
+    markdownFile = sys.argv[1]
+    htmlFile = sys.argv[2]
 
 
-if not os.path.isfile(markdownFile):
-    """check markdown if it is a markdownfile"""
-    sys.stderr.write(f'Missing {markdownFile}\n')
-    sys.exit(1)
+    if not os.path.isfile(markdownFile):
+        """check markdown if it is a markdownfile"""
+        sys.stderr.write(f'Missing {markdownFile}\n')
+        sys.exit(1)
 
 sys.exit(0)
