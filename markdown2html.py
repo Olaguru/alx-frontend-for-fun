ba9facff669e3907083e444c9ef19b35ffeb331e
olaguru <dapoodedeyi03@gmail.com>
2024-10-21 06:35:34 -0700

    ai

--- markdown2html.py
+++ markdown2html.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 """
-    Markdown to Html
-    a script markdown2html.py that takes an argument 2 strings:
-    First argument is the name of the Markdown file
-    Second argument is the output file name
+Markdown to HTML conversion script.
+A script markdown2html.py that takes 2 arguments:
+1. The name of the Markdown file
+2. The output HTML file name
 """
 import sys
 import os
-""" sys for args and os to check file"""
 
-
+# Check if the number of arguments is exactly 3
 if len(sys.argv) != 3:
-    """ check the arg len """
+    # Print usage to STDERR and exit with code 1
     sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
     sys.exit(1)
 
+# Assign arguments to variables
 markdownFile = sys.argv[1]
 htmlFile = sys.argv[2]
 
-
+# Check if the Markdown file exists
 if not os.path.isfile(markdownFile):
-    """check markdown if it is a markdownfile"""
+    # Print an error message if the file is missing and exit with code 1
     sys.stderr.write(f'Missing {markdownFile}\n')
     sys.exit(1)
 
+# If the script runs without any issue, exit with success code 0
 sys.exit(0)
